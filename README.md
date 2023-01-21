04ddb4fd546078f1b1dc3845de6c2822a83d3e37
Steven Robertson <stever@hey.com>
2023-01-21 19:12:33 +0000

    Use Python to serve, consistent with other examples

--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 
 WebAssembly example using [TinyGo](https://tinygo.org/getting-started/install/).
 
-```bash
-go run main.go
-```
-
 Recompiling the `main.wasm` file:
 
 ```bash
 tinygo build -o html/main.wasm -target wasm html/main.go
+```
+
+Serve via HTTP:
+
+```bash
+python -m http.server 8000
 ```
 
 Based on example provided by Aaron Turner and licensed under the
--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"strings"
-)
+// Declare a main function, this is the entrypoint into our go module
+// That will be run. In our example, we won't need this
+func main() {}
 
-const dir = "./html"
 
-func main() {
-	fs := http.FileServer(http.Dir(dir))
-	log.Print("Serving " + dir + " on http://localhost:8080")
-	http.ListenAndServe(":8080", http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-		resp.Header().Add("Cache-Control", "no-cache")
-		if strings.HasSuffix(req.URL.Path, ".wasm") {
-			resp.Header().Set("content-type", "application/wasm")
-		}
-		fs.ServeHTTP(resp, req)
-	}))}
+// This exports an add function.
+// It takes in two 32-bit integer values
+// And returns a 32-bit integer value.
+// To make this function callable from JavaScript,
+// we need to add the: "export add" comment above the function
+//export add
+func add(x int, y int) int {
+  return x + y;
+}
